1455f70965ac49209750d7cb9796bd3154252774
Ross Harding <ross@ponto.org>
2023-01-11 13:53:11 -0800

    clean up

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // socketMode: true,
-  // appToken: process.env.SLACK_APP_TOKEN,
 });
 
 app.command("/tldr", async ({ command, ack, client }) => {

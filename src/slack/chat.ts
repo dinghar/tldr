3138a139cfe99de7646ada5b7f2bd7d678196c38
Ross Harding <ross@ponto.org>
2023-01-31 18:26:34 -0800

    Change timeframe behavior

--- src/slack/chat.ts
+++ src/slack/chat.ts
@@ -52,9 +52,33 @@ export function sendSummary(say, text) {
 }
 
 export function sendInvalidTimeframeResponse(client, command) {
+  sendBasicMessage(
+    client,
+    command,
+    "Sorry, I didn't understand that timeframe. Please use a format like '1d' or '2 hours'"
+  );
+}
+
+export function sendEmptyTimeframeResponse(client, command) {
+  sendBasicMessage(
+    client,
+    command,
+    "Please pass a timeframe, like '1d' or '2 hours'"
+  );
+}
+
+export function sendEmptyMessagesResponse(client, command) {
+  sendBasicMessage(
+    client,
+    command,
+    "No messages found for the timeframe you entered."
+  );
+}
+
+function sendBasicMessage(client, command, text: string) {
   client.chat.postEphemeral({
     channel: command.channel_id,
     user: command.user_id,
-    text: "Sorry, I didn't understand that timeframe. Please use a format like '1d' or '2 hours'",
+    text: text,
   });
 }
--- src/summaryGenerator.ts
+++ src/summaryGenerator.ts
@@ -3,7 +3,12 @@ import { generateSummaryPreviewText } from "./utils/strings";
 import { fetchAISummary } from "./openaiClient";
 import { fetchHistory } from "./slack/history";
 import { parseParams } from "./utils/parse-params";
-import { sendInvalidTimeframeResponse, sendSummaryPreview } from "./slack/chat";
+import {
+  sendEmptyMessagesResponse,
+  sendEmptyTimeframeResponse,
+  sendInvalidTimeframeResponse,
+  sendSummaryPreview,
+} from "./slack/chat";
 import { formatDate } from "./utils/date-formatter";
 import { generateConstraintString } from "./utils/strings";
 import { MILLISECONDS_PER_DAY } from "./constants";
@@ -18,28 +23,27 @@ export async function processTldrRequest(command, client, userId, channelId) {
       return;
     }
 
-    let oldest = generateTimeConstraint(timeframe);
+    // Handle lack of params
+    if (!timeframe) {
+      sendEmptyTimeframeResponse(client, command);
+      return;
+    }
+
+    const oldest = generateTimeConstraint(timeframe);
     const maxMessages = 200;
-    let retryCount = 7;
-    let history;
+    const history = await fetchHistory(client, channelId, oldest, maxMessages);
 
-    // TODO: This all does not currently work, need to debug
+    const messages = history.messages;
 
-    /* 
-      Fetch history, then check if the message array is empty. If so, and
-      if no timeframe was passed, retry day by day up to one week.
-     */
-    do {
-      history = await fetchHistory(client, channelId, oldest, maxMessages);
-      oldest = (oldest - MILLISECONDS_PER_DAY) / 1000;
-      retryCount--;
-    } while (!timeframe && history.messages.count === 0 && retryCount);
+    if (!messages || messages.length === 0) {
+      sendEmptyMessagesResponse(client, command);
+      return;
+    }
 
     const startDate = formatDate(oldest * 1000);
     const constraintString = generateConstraintString(maxMessages, startDate);
 
-    // TODO determine if this can be removed or worked around
-    const sortedMessages = sortMessages(history.messages);
+    const sortedMessages = sortMessages(messages);
     const transcript = await generateSummaryTranscript(
       sortedMessages,
       client,
@@ -58,9 +62,6 @@ function generateSummaryTranscript(
   constraintString
 ): Promise<string> {
   return new Promise((resolveWholeFn) => {
-    if (messages.count === 0) {
-      resolveWholeFn("No messages found for the timeframe you entered");
-    }
     const userIds = new Set();
     const identities = [];
 
